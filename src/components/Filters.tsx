882130c3692af6043d9e925b063371ec17d26274
Edwin <edgoca2498@gmail.com>
2024-07-21 21:36:27 -0600

    chore: Work in progress select 'sort' filter component

---
+++ src/components/Filters.tsx
@@ -0,0 +1,39 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+const Filters = () => {
+  return (
+    <div className="flex flex-col space-y-2">
+      {/* TODO: Sort By */}
+      <div className="relative">
+        <label className="block mb-2 text-gray-200">Sort by</label>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Fruits</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      {/* TODO: Region */}
+      {/* TODO: Status */}
+    </div>
+  );
+};
+
+export default Filters;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,26 @@
-import Search from '../components/Search';
+import Search from '@/components/Search';
+import Filters from '@/components/Filters';
 
 const HomePage = () => {
   return (
     <div className="max-w-7xl mx-auto -mt-16 bg-dark-200 min-h-screen rounded-lg p-6">
       <div className="flex justify-between">
-        <h5>Founds 232 countries</h5>
+        <h5 className='text-base text-gray-200 font-semibold'>Founds 232 countries</h5>
         <div className='w-80'>
           <Search
             id="search-input"
             placeholder="Search by Name, Region, Subregion"
             onChange={() => {}}
           />
+        </div>
+      </div>
+      {/* Section */}
+      <div className='flex flex-row space-x-4 mt-6'>
+        <div className='w-1/4'>
+          <Filters />
+        </div>
+        <div className='w-3/4'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam incidunt vel veniam nobis rem perspiciatis delectus enim excepturi ipsam sit modi quia, odio minima natus nam vero dolorum doloremque quibusdam.
         </div>
       </div>
     </div>
